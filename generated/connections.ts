efdd88af3e63ba61049dc01592acd0b5c7ad82ad
Terminal Droid <121902293+terminal-droid@users.noreply.github.com>
2026-02-23 21:21:15 -0500

    chore: sync OpenAPI spec from upstream (#3)
    Co-authored-by: MisterJimson <7351329+MisterJimson@users.noreply.github.com>

--- generated/connections.ts
+++ generated/connections.ts
@@ -561,6 +561,14 @@ export const commands: Command[] = [
               description: "If the sync failed, this will contain the reason",
               example: "Reason for failure if sync status is 'failed'",
             },
+            progress: {
+              type: "number",
+              title: "Percentage",
+              description: "Percentage value between 0 and 100, rounded to 2 decimal places.",
+              example: 85,
+              minimum: 0,
+              maximum: 100,
+            },
             issues: {
               type: "array",
               description:
